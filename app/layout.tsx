4dfeb44934895641deb562423ea9f6a0ddc0e79c
tauhid97k <tauhid.coder@gmail.com>
2024-05-18 15:13:26 +0600

    Top progress bar added on route change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ToastContainer } from '@/components/ui/toaster'
+import NextTopLoader from 'nextjs-toploader'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader showSpinner={false} />
           {children}
           <ToastContainer />
         </ThemeProvider>
